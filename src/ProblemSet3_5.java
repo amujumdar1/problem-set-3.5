b14f50816ed568a9f6aba25da0cdb8427d091624
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2018-10-26 11:23:48 -0400

    final hopefully

--- src/ProblemSet3_5.java
+++ src/ProblemSet3_5.java
@@ -21,7 +21,11 @@ public class ProblemSet3_5 {
 		
 		// test your solutions here
 		
-		ps.testFunctions();
+		ps.testPrimes();
+		ps.testLeapYears();
+		ps.testPalindromicNumbers();
+		ps.testFibonacci();
+		ps.testMultiples();
 		
 	}
 	
@@ -105,23 +109,21 @@ public class ProblemSet3_5 {
 		System.out.print("The next " + count + " leap years are ");
 		
 		
-		for (int x = 0; x < 4 * (count - 2); x+=4) {
+		int x = 0;
+				
+		while (x < count) { // not a for loop because not always incrementing
 			
-			if (isLeap(newYear + x)) System.out.print((newYear + x) + ", ");
-			else count++;
-			/* loop to add 4 until the last two years
-			 * if it doesn't follow the condition, though adds 1 to count
-			 */
+			if (x > 0) newYear += 4;
+			
+			if (isLeap(newYear)) {
+				
+				x++; 
+				
+				if (x != count) System.out.print(newYear + ", ");
+				 
+				else System.out.println("and " + newYear + ".");
+			}
 		}
-		
-		int finalYear = newYear + (count * 4 - 4);
-		// so that the "and" is consistent 
-		
-		if (!isLeap(finalYear)) System.out.println("and " + (finalYear) + ".");
-		// in the rare case that the last number is 2100 or something like that
-		
-		else System.out.println((finalYear - 4) + ", and " + (finalYear) + ".");
-		
 		
 	}
 	
@@ -233,14 +235,244 @@ public class ProblemSet3_5 {
 		System.out.println("The sum of all multiples of " + x + " and " + y + " less than " + limit + " is " + sum + ".");
 		
 	}
+	private void testPrimes() {		
+		System.out.print("primes(1, 2), expected       : There is 1 prime number.\n");
+		System.out.print("primes(1, 2), actual         : ");
+		primes(1, 2);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 10), expected      : There are 4 prime numbers.\n");
+		System.out.print("primes(1, 10), actual        : ");
+		primes(1, 10);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 100), expected     : There are 25 prime numbers.\n");
+		System.out.print("primes(1, 100), actual       : ");
+		primes(1, 100);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 1000), expected    : There are 168 prime numbers.\n");
+		System.out.print("primes(1, 1000), actual      : ");
+		primes(1, 1000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 10000), expected   : There are 1229 prime numbers.\n");
+		System.out.print("primes(1, 10000), actual     : ");
+		primes(1, 10000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 100000), expected  : There are 9592 prime numbers.\n");
+		System.out.print("primes(1, 100000), actual    : ");
+		primes(1, 100000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("primes(1, 1000000), expected : There are 78498 prime numbers.\n");
+		System.out.print("primes(1, 1000000), actual   : ");
+		primes(1, 1000000);
+		System.out.println("------------------------------------------------------------------");
+	}
 	
+	/*
+	 * Private method designed to test the implementation of leapYears.
+	 */
 	
-	public void testFunctions() {
+	private void testLeapYears() {
+		System.out.print("leapYears(-1), expected : I don't know how to compute the next -1 leap years...\n");
+		System.out.print("leapYears(-1), actual   : ");
+		leapYears(-1);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("leapYears(0), expected  : I don't know how to compute the next 0 leap years...\n");
+		System.out.print("leapYears(0), actual    : ");
+		leapYears(0);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("leapYears(1), expected  : The next leap year is 2020.\n");
+		System.out.print("leapYears(1), actual    : ");
+		leapYears(1);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("leapYears(2), expected  : The next 2 leap years are 2020 and 2024.\n");
+		System.out.print("leapYears(2), actual    : ");
+		leapYears(2);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("leapYears(10), expected : The next 10 leap years are 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052, and 2056.\n");
+		System.out.print("leapYears(10), actual   : ");
+		leapYears(10);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("leapYears(26), expected : The next 26 leap years are 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088, 2092, 2096, 2104, 2108, 2112, 2116, 2120, and 2124.\n");
+		System.out.print("leapYears(26), actual   : ");
+		leapYears(26);
+		System.out.println("------------------------------------------------------------------");
+	}
+	
+	/*
+	 * Private method designed to test the implementation of palindromicNumbers.
+	 */
+	
+	private void testPalindromicNumbers() {
+		System.out.print("palindromicNumbers(1), expected         : 1 is a palindromic number.\n");
+		System.out.print("palindromicNumbers(1), actual           : ");
+		palindromicNumbers(1);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("palindromicNumbers(101), expected       : 101 is a palindromic number.\n");
+		System.out.print("palindromicNumbers(101), actual         : ");
+		palindromicNumbers(101);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("palindromicNumbers(12321), expected     : 12321 is a palindromic number.\n");
+		System.out.print("palindromicNumbers(12321), actual       : ");
+		palindromicNumbers(12321);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("palindromicNumbers(12345), expected     : 12345 is not a palindromic number.\n");
+		System.out.print("palindromicNumbers(12345), actual       : ");
+		palindromicNumbers(12345);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("palindromicNumbers(12), expected        : 12 is not a palindromic number.\n");
+		System.out.print("palindromicNumbers(12), actual          : ");
+		palindromicNumbers(12);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("palindromicNumbers(123454321), expected : 123454321 is a palindromic number.\n");
+		System.out.print("palindromicNumbers(123454321), actual   : ");
+		palindromicNumbers(123454321);
+		System.out.println("------------------------------------------------------------------");
+	}
+	
+	/*
+	 * Private method designed to test the implementation of fibonacci.
+	 */
+	
+	private void testFibonacci() {
+		System.out.print("fibonacci(1), expected  : The 1st Fibonacci number is 1.\n");
+		System.out.print("fibonacci(1), actual    : ");
+		fibonacci(1);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(2), expected  : The 2nd Fibonacci number is 1.\n");
+		System.out.print("fibonacci(2), actual    : ");
+		fibonacci(2);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(3), expected  : The 3rd Fibonacci number is 2.\n");
+		System.out.print("fibonacci(3), actual    : ");
+		fibonacci(3);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(4), expected  : The 4th Fibonacci number is 3.\n");
+		System.out.print("fibonacci(4), actual    : ");
+		fibonacci(4);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(10), expected : The 10th Fibonacci number is 55.\n");
+		System.out.print("fibonacci(10), actual   : ");
+		fibonacci(10);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(21), expected : The 21st Fibonacci number is 10946.\n");
+		System.out.print("fibonacci(21), actual   : ");
+		fibonacci(21);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(22), expected : The 22nd Fibonacci number is 17711.\n");
+		System.out.print("fibonacci(22), actual   : ");
+		fibonacci(22);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(23), expected : The 23rd Fibonacci number is 28657.\n");
+		System.out.print("fibonacci(23), actual   : ");
+		fibonacci(23);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(24), expected : The 24th Fibonacci number is 46368.\n");
+		System.out.print("fibonacci(24), actual   : ");
+		fibonacci(24);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(25), expected : The 25th Fibonacci number is 75025.\n");
+		System.out.print("fibonacci(25), actual   : ");
+		fibonacci(25);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(50), expected : The 50th Fibonacci number is 12586269025.\n");
+		System.out.print("fibonacci(50), actual   : ");
+		fibonacci(50);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("fibonacci(75), expected : The 75th Fibonacci number is 2111485077978050.\n");
+		System.out.print("fibonacci(75), actual   : ");
+		fibonacci(75);
+		System.out.println("------------------------------------------------------------------");
+	}
+	
+	/*
+	 * Private method designed to test the implementation of testMultiples.
+	 */
+	
+	private void testMultiples() {
+		System.out.print("multiples(2, 4, 10), expected : The sum of all multiples of 2 and 4 less than 10 is 20.\n");
+		System.out.print("multiples(2, 4, 10), actual   : ");
+		multiples(2, 4, 10);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(3, 5, 10), expected : The sum of all multiples of 3 and 5 less than 10 is 23.\n");
+		System.out.print("multiples(3, 5, 10), actual   : ");
+		multiples(3, 5, 10);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(2, 4, 100), expected : The sum of all multiples of 2 and 4 less than 100 is 2450.\n");
+		System.out.print("multiples(2, 4, 100), actual   : ");
+		multiples(2, 4, 100);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(3, 5, 100), expected : The sum of all multiples of 3 and 5 less than 100 is 2318.\n");
+		System.out.print("multiples(3, 5, 100), actual   : ");
+		multiples(3, 5, 100);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(2, 4, 1000), expected : The sum of all multiples of 2 and 4 less than 1000 is 249500.\n");
+		System.out.print("multiples(2, 4, 1000), actual   : ");
+		multiples(2, 4, 1000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(3, 5, 1000), expected : The sum of all multiples of 3 and 5 less than 1000 is 233168.\n");
+		System.out.print("multiples(3, 5, 1000), actual   : ");
+		multiples(3, 5, 1000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(2, 4, 10000), expected : The sum of all multiples of 2 and 4 less than 10000 is 24995000.\n");
+		System.out.print("multiples(2, 4, 10000), actual   : ");
+		multiples(2, 4, 10000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(3, 5, 10000), expected : The sum of all multiples of 3 and 5 less than 10000 is 23331668.\n");
+		System.out.print("multiples(3, 5, 10000), actual   : ");
+		multiples(3, 5, 10000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(2, 4, 100000), expected : The sum of all multiples of 2 and 4 less than 100000 is 2499950000.\n");
+		System.out.print("multiples(2, 4, 100000), actual   : ");
+		multiples(2, 4, 100000);
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.print("multiples(3, 5, 100000), expected : The sum of all multiples of 3 and 5 less than 100000 is 2333316668.\n");
+		System.out.print("multiples(3, 5, 100000), actual   : ");
+		multiples(3, 5, 100000);
+		System.out.println("------------------------------------------------------------------");
+	}
+	
+	/*public void testFunctions() {
 		primes(1, 342342);
 		multiples(1, 2, 5); 
 		palindromicNumbers(14511541);
 		palindromicNumbers(1);
-		leapYears(20);
+		fibonacci(50);
+		leapYears(21);
 		multiples(3, 5, 2000);
-	}
+	}*/
 }
